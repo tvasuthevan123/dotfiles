fc7374e6b7e35905b9808e48bb35f1f94d2c7b16
Thanuj V <thanuj@myurbanjungle.com>
2024-08-25 21:27:45 +0100

    Finalise nvim/tmux setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
-echo "Creating symbolic links for nvim, tmux"
+echo "Create .config directory"
 mkdir -p ~/.config
 
+echo "Symlinks"
 ln -sf "$(pwd)/dotfiles/nvim" ~/.config
 ln -sf "$(pwd)/dotfiles/tmux" ~/.config
 ln -sf "$(pwd)/dotfiles/fish" ~/.config
 
-echo "Cloning tpm"
+echo "Reset TMUX Plugins Folder"
 rm -rf ~/.config/tmux/plugins/
+mkdir -p ~/.config/tmux/plugins/
 
+echo "Clone TPM"
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 echo "Install TMUX plugins"
