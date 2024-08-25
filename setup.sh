6e0b4c56c5f862a81e24e725f7a735e75eef16d5
Thanuj V <thanuj@myurbanjungle.com>
2024-08-25 20:23:52 +0100

    Move messing around

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ rm -rf ~/.config/tmux/plugins/
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/
 
 echo "InstalL TMUX plugins"
-tmux -f ~/.config/tmux/.tmux.conf start-server
+tmux start-server
 tmux new-session -d -A -s main
 ~/.config/tmux/plugins/bin/install_plugins
 tmux kill-server
